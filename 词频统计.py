115438330196c1042172e7ed99e4b93ce96814c7
gitlGl <43978320+gitlGl@users.noreply.github.com>
2023-10-30 01:21:15 +0800

    Update 词频统计.py

--- 词频统计.py
+++ 词频统计.py
@@ -1,5 +1,6 @@
 # 导入依赖
-import jieba,os,psutil,xlrd,os
+import os,psutil,xlrd,os
+#import jieba
 import pandas as pd
 import multiprocessing
 
@@ -21,7 +22,7 @@ def getKeyWordData(text_path,filer_name,key_word):
     with  open(path_filename, "r", encoding='utf-8') as f:
         txt = f.read()
         # 使用精确模式对文本进行分词
-        words = jieba.lcut(txt)
+        #words = jieba.lcut(txt)
         # 通过键值对的形式存储词语及其出现的次数
         year = filer_name[:-8]
         year  = year[-4:]#获取年份
@@ -29,7 +30,7 @@ def getKeyWordData(text_path,filer_name,key_word):
         key_word_data['公司代码'] = filer_name+'年报'
         key_word_data['年份'] = year
         for wd in key_word:
-            key_word_data[wd] = words.count(wd)#统计关键词出现次数
+            key_word_data[wd] = txt.count(wd)#统计关键词出现次数
     return key_word_data   
 def statistics(folder_name,key_word):
     list_filename = os.listdir(base_dir+folder_name)
