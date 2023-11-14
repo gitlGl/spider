eb9af79bfb95e3272e6d0a85f51044a5faa6f56a
gitlG <1637885653@qq.com>
2023-11-14 13:01:47 +0800

    new

--- 词频/pdf2txt.py
+++ 词频/pdf2txt.py
@@ -1,26 +1,29 @@
 from pdfminer.high_level import extract_text
-import os ,psutil
+import os ,psutil,sys
+os.chdir(sys.path[0])
 import multiprocessing          
-def pdf2txt(folder_name):#转换函数,多进程调用函数
-    list_filename = os.listdir(folder_name)
-    for filename in list_filename:
-            if  filename.endswith(".PDF")  or filename.endswith(".pdf"):
-                print(folder_name+filename+":转换中......")
-                text = extract_text(folder_name+filename)
-                with open(folder_name+filename[:-4]+".txt", "w", encoding="utf-8") as f:
-                    f.write(text)
-                print(folder_name+filename+":转换成功")
-                os.remove(folder_name+filename)#转换成功后删除pdf文件
+def pdf2txt(file_name_lst):#转换函数,多进程调用函数
+    for filename in file_name_lst:
+        if  filename.endswith(".PDF")  or filename.endswith(".pdf"):
+            print(folder_name+filename+":转换中......")
+            text = extract_text(folder_name+filename)
+            with open(folder_name+filename[:-4]+".txt", "w", encoding="utf-8") as f:
+                f.write(text)
+            print(folder_name+filename+":转换成功")
+            os.remove(folder_name+filename)#转换成功后删除pdf文件
 
 def main():#使用多进程，提高转换速度
     base_dir = "出口上市公司年报/"
-    list_folder_name = next(os.walk(base_dir))[1]
     pool = multiprocessing.Pool(processes = psutil.cpu_count()+1)
-    for folder_name in list_folder_name:
-        pool.apply_async(pdf2txt, (base_dir+folder_name+'/',))
+    for _,folder_name_lst,file_name_lst in os.walk(base_dir):
+        pool.apply_async(pdf2txt, (file_name_lst,))
+        
     pool.close()
     pool.join()
 
 if __name__ == '__main__':
-    main()
+    #main()
+    test = os.walk(r"C:\Users\Administrator\Desktop\金融")
+    print(next(test))
+    print(next(test))
     
--- 词频/词频统计.py
+++ 词频/词频统计.py
@@ -1,17 +1,19 @@
 # 导入依赖
-import os,psutil,xlrd
+import os,psutil,xlrd,sys
 import pandas as pd
 import multiprocessing
+#import jieba
 # 获取关键词列表
+os.chdir(sys.path[0])
 def getKeyWordList():
     key_word = []
     with xlrd.open_workbook(keyword_dir) as book:
             sheets = book.sheets()
             for sheet in sheets:
                 rows = sheet.nrows
-                for i in range(rows):
-                    list1 = sheet.row_values(rowx=i)
-                    key_word.append((list1[0]))
+            for i in range(rows):
+                list1 = sheet.row_values(rowx=i)
+                key_word.append((list1[0]))
     return key_word
 def getKeyWordData(text_path,filer_name,key_word):
     # 读取文本
@@ -28,12 +30,12 @@ def getKeyWordData(text_path,filer_name,key_word):
         key_word_data['公司代码'] = filer_name+'年报'
         key_word_data['年份'] = year
         for wd in key_word:
-            key_word_data[wd] = txt.count(wd)#统计关键词出现次数
+            key_word_data[wd] =txt.count(wd)#统计关键词出现次数
     return key_word_data   
-def statistics(folder_name,key_word):
-    list_filename = os.listdir(base_dir+folder_name)
+def statistics(file_name_lst,key_word):
+    
     datas = []
-    for filename in list_filename:
+    for filename in file_name_lst:
         if filename.endswith(".txt"):
             key_word_data = getKeyWordData(base_dir+folder_name+"/",filename,key_word)
             #os.remove("年度报告/"+folder_name+"/"+filename)
@@ -49,24 +51,27 @@ def statistics(folder_name,key_word):
         df.to_csv(cipin_dir, mode='a', index=False,header=True,encoding="gbk")
     else:
         df.to_csv(cipin_dir, mode='a', index=False, header=False,encoding="gbk")
-    for filename in list_filename:
-        if filename.endswith(".txt"):
-            pass   
-            #os.remove(base_dir+folder_name+"/"+filename)
+    for filename in file_name_lst:
+        if filename.endswith(".txt"):   
+            os.remove(base_dir+folder_name+"/"+filename)
        
 # 主函数
 def main():
-    list_folder_name = next(os.walk(base_dir))[1]
     key_word = getKeyWordList()
     pool = multiprocessing.Pool(processes = psutil.cpu_count()+1)#使用多进程，提高统计速度
-    for folder_name in list_folder_name:
-        pool.apply_async(statistics, (folder_name,key_word))#一个进程处理一个文件夹
-    pool.close()
+    for _,folder_name_lst,file_name_lst in os.walk(base_dir):
+        pool.apply_async(statistics, (file_name_lst,key_word))#
+
     pool.join()
+    pool.close()
+
 base_dir = "出口上市公司年报/"
 keyword_dir =  "关键词.xls"
 cipin_dir =  "词频统计.csv"
 
+
 if __name__ == '__main__':
     main()
-    #utf8_to_gbk(cipin_dir)
+   
+
+
