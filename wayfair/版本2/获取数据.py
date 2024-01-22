f94acd6f5ea61774fff3c234693ab03641f47a1d
gitlG <1637885653@qq.com>
2024-01-22 15:05:14 +0800

    new

--- wayfair/版本2/获取数据.py
+++ wayfair/版本2/获取数据.py
@@ -160,7 +160,7 @@ def run(playwright: Playwright,sheet_names) -> None:
                     page.locator('input[name="poNumber"]').fill(po_number)
                     page.locator('button:text("Search")').click()
                     page.wait_for_load_state("networkidle")
-                    random_sleep(2,3)
+                    random_sleep(0.5,1)
                     
                     div_rt_tbody = page.query_selector('div.rt-tbody')  
                     div_rt_tr_groups = div_rt_tbody.query_selector_all('div.rt-tr-group')
