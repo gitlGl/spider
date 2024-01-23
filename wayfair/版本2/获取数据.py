b6cbcdeeb0a9485ed7b2e225dece091a75b1611d
gitlG <1637885653@qq.com>
2024-01-23 14:46:11 +0800

    测试点1

--- wayfair/版本2/获取数据.py
+++ wayfair/版本2/获取数据.py
@@ -1,9 +1,10 @@
 
 from playwright.sync_api import Playwright, sync_playwright
 from openpyxl import load_workbook
-import time,os,random,csv,math
+import time,os,random,csv
 from decimal import Decimal
 from copy import deepcopy
+
 current_file_path = os.path.abspath(__file__)
 os.chdir(os.path.dirname(current_file_path))  
 
@@ -11,12 +12,11 @@ def random_sleep(min_time, max_time):
     sleep_time = random.uniform(min_time, max_time)
     time.sleep(sleep_time)
     
-
-
 def readTxt(process_file):# 读取已下载的公司代码
     if not os.path.exists(process_file):
         with open(process_file, "w") as f:
             f.write('')
+            
     with open(process_file, "r") as f:
         data = f.read().splitlines()
         return set(data) 
@@ -29,7 +29,6 @@ def get_po_num(local_file,sheet_name):
     return [str(cell.value) for cell in sheet['A'] if cell.value ]
 
 def get_number(tem_number):  
-    
     number = ''
     for i in tem_number:
         if not i.isdigit():
@@ -37,11 +36,9 @@ def get_number(tem_number):
         number = number + i
         if len(number) == 9:
             return number
+        
     return number
   
-  
-#   with open(detail_data_file,"a+",newline = '') as csv_f:
-#                                 csv.writer(csv_f).writerow([po_number,0])
 def clear_row_data(local_file,sheet_name,row_data_file,
                    detail_data_file,server_file ):
     row = get_po_num(local_file,sheet_name)
@@ -74,8 +71,7 @@ def clear_row_data(local_file,sheet_name,row_data_file,
                 set__.add("".join(i))
             else:
                 target.append("可能存在问题")
-            
-                
+             
         sum_money = sum([Decimal(data[-2][1:].replace(",","")) for data in  value if data] )
         sum_money = float(sum_money)
         target.insert(0,str(sum_money))
@@ -88,23 +84,19 @@ def clear_row_data(local_file,sheet_name,row_data_file,
             for i in value:
                 csv.writer(csv_f).writerow(i)
 
-    
-        
-    
 # 获取CSV文件数据
 def get_row_data(row_data_file):
     with open(row_data_file, 'r') as file:
         reader = csv.reader(file)
-        
         list_data = []
+        
         for row in reader:
             if len(row)<2:
                 print(row)
             list_data.append(row)
+            
         return list_data   
         
-    
-    
 def get_set_data(fiel_path):
         if not os.path.exists(fiel_path):
             with open(fiel_path, "w") as f:
@@ -117,30 +109,80 @@ def get_set_data(fiel_path):
                 tem_number = get_number(''.join(row))
                 set_.add(tem_number)
             return set_
-          
+        
+def get_text(page,po_number,set_data,row_data_file,error_data):
+    while True:
+        div_rt_tbody = page.query_selector('div.rt-tbody')  
+        div_rt_tr_groups = div_rt_tbody.query_selector_all('div.rt-tr-group')
+        tt = []  
+        data_list = []
+        count = 0
+        
+        for div_rt_td in div_rt_tr_groups:
+            div_datas = div_rt_td.query_selector_all('div.rt-td')
+            data_ = []
+            
+            for index,item in enumerate( div_datas):
+                text = item.text_content()
+                if text != "\xa0" and  text!="":
+                    if index == 2:
+                        tem_number = get_number(text)
+                        if po_number[2:] != tem_number:
+                            tt.append(False)
+                        else:
+                            tt.append(True)
+                            
+                    data_.append(text)
+                    tt.append(True)
+              
+                else:
+                    tt.append(False)
+                    
+            data_list.append(data_)
+                    
+        if all(tt) and len(tt)!= 0:
+            for i in data_list: 
+                i.append(tem_number)
+                string = "".join(i)
+                if string not in set_data:
+                    set_data.add(string)
+                    with open(row_data_file,"a+",newline = '') as csv_f1:
+                        csv.writer(csv_f1).writerow(i)
+                        
+                else:
+                    if string not in error_data:
+                        error_data.add(string)
+                        with open("重点核查数据.csv","a+",newline = '') as csv_f2:
+                            csv.writer(csv_f2).writerow(i)
+            return True                   
+                        
+        else:
+            count = count +1
+            print(f" 尝试获取数据失败次数{po_number}：{count}")
+            if count > 100:
+                return False
+
 def run(playwright: Playwright,sheet_names) -> None:
     browser = playwright.firefox.launch(headless=True)
     # 加载本地cookies，免登陆
     context = browser.new_context(storage_state="state.json")
     # 打开页面继续操作
     page = context.new_page()
+    
     for sheet_name in sheet_names:
         server_file = f"{sheet_name}.csv"
-       
         process_file = f"{sheet_name}.txt"
         row_data_file = f"{sheet_name}-row_data.csv"
         detail_data_file = f"{sheet_name}-detail.csv"
         process_num = readTxt(process_file)
         list_number = get_po_num(local_file,sheet_name)
         set_data = get_set_data(row_data_file)
-        importtance_may_error = get_set_data("重点核查数据.csv")
+        error_data = get_set_data("重点核查数据.csv")
         
         for po_number in list_number[:]:
             if po_number in process_num:
                 list_number.remove(po_number)
                 print(f"查询：{po_number}，跳过")
-    
-       
     
         while True:
             try:
@@ -150,7 +192,6 @@ def run(playwright: Playwright,sheet_names) -> None:
             except Exception as e :
                 print("异常网络，重试",e.__traceback__.tb_lineno,e)
                 random_sleep(0.8,1)
-    
     
         for po_number in list_number:
             count = 0
@@ -162,61 +203,27 @@ def run(playwright: Playwright,sheet_names) -> None:
                     page.wait_for_load_state("networkidle")
                     random_sleep(0.5,1)
                     
-                    div_rt_tbody = page.query_selector('div.rt-tbody')  
-                    div_rt_tr_groups = div_rt_tbody.query_selector_all('div.rt-tr-group')
+                    flag = get_text(page,po_number,set_data,row_data_file,error_data)
                     
-                    tt = []  
-                    flag = 0
-                    for div_rt_td in div_rt_tr_groups:
-                        div_datas = div_rt_td.query_selector_all('div.rt-td')
-                        data_ = []
-                        for item in div_datas:
-                        
-                            text = item.text_content()
-                            if text != "\xa0":
-                                data_.append(text)
-                                tt.append(text)
-                        if data_:
-                            if len(data_)>3:
-                                tem_number = get_number(data_[2])
-                                if po_number[2:] == tem_number:
-                                    flag = 1
-                                    
-                                data_.append(tem_number)
-                                string = "".join(data_)
-                                if string not in set_data:
-                                    set_data.add(string)
-                                    with open(row_data_file,"a+",newline = '') as csv_f1:
-                                        csv.writer(csv_f1).writerow(data_)
-                                        
-                                else:
-                                    if string not in importtance_may_error:
-                                        importtance_may_error.add(string)
-                                        with open("重点核查数据.csv","a+",newline = '') as csv_f2:
-                                            csv.writer(csv_f2).writerow(data_)
-                                            
-                                    
-                
-                    if not tt or not flag:
-                        count = count +1
-                        print(f" {po_number} count:",count)
-                        if count > 100:
-                            count = 0
-                            break
-                       
-                        continue
-                
-                    with open(process_file, "a") as f:
-                        f.write(f"{po_number}\n") # 将内容追加到到文件尾部
-                    print(f"进度{po_number}")
-                   
-                    break                  
+                    if flag:
+                        with open(process_file, "a") as f:
+                            f.write(f"{po_number}\n") # 将内容追加到到文件尾部
+                            print(f"{po_number}:获取数据成功")
+                        break
+                    
+                    else:
+                        count = count + 1
+                        print(f"重新查询次数{po_number}：{count}")
+                        if count > 10:
+                            print(f"{po_number}:获取数据失败")
+                            with open(process_file, "a") as f:
+                                f.write(f"{po_number}\n") # 将内容追加到到文件尾部
+                                print(f"进度{po_number}")
+                            break         
                 
                 except Exception as e:
                     print("异常网络，重试",e.__traceback__.tb_lineno,e,e.__traceback__.tb_frame)
-                  
-                    page.locator('input[name="poNumber"]').clear()
-                    
+                 
         with open("sheet_name.txt", "a+",encoding="utf8") as f:
             f.seek(0)
             data = f.read().splitlines()
@@ -230,18 +237,13 @@ def run(playwright: Playwright,sheet_names) -> None:
                 f.seek(0,2)
                 f.write(f"{sheet_name}\n") # 将内容追加到到文件尾部
                 print(f"进度{sheet_name}")
-                
-            
-    
-       
+                   
     context.close()
     browser.close()
-
 
 if "__main__" == __name__:
     local_file = "Testing11.20-11.30.xlsx"
     workbook = load_workbook(local_file)
     sheet_names = workbook.sheetnames
-    
     with sync_playwright() as playwright:
         run(playwright,sheet_names)
